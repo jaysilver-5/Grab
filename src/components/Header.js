679fcc9eb3f5f2ac72603b985a40c141ce272c37
Joel Udo <udojoshua3@gmail.com>
2023-03-29 10:35:16 +0100

    few changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Logo from './assets/Logo.png';
+import Time from './assets/Time.png'
 
-function Header() {
+// const Logo = function() {
+//   return(
+//     <div className="flex fixed justify-between mx-[15%] w-[20px] z-10 h-[20px]">
+//       <div className='transform bg-primary -rotate-45 absolute top-0 left-0 w-[8px] h-[8px]'></div>
+//       <div class="absolute top-0 bg-primary right-0 w-[8px] h-[8px]"></div>
+//       <div class="absolute bottom-0 bg-primary left-0 w-[8px] h-[8px]"></div>
+//       <div class="absolute bottom-0 bg-primary right-0 w-[8px] h-[8px]"></div>
+//       <div className='grid place-items-center flex gap-4 ml-[30px]'>
+//         <h1 className='grid place-items-center text-2xl text-white font-semibold font-primary mb' >Transitflow</h1>
+//       </div>
+//     </div>
+//   )
+// }
+
+export default function Header() {
   return (
-    <Header>
-        <div className="mx-auto bg-secondary py-8">
-            <div flex>
-                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <header className='py-8 bg-secondary'>
+        <div className="mx-auto">
+            <div className="flex mx-[17%] items-center justify-start">
+              {/* logo */}
+                <img src={Logo} alt='' className='h-[36px] w-[210px]'></img>
+
+
+              <img src={Time} alt="" className='ml-auto w-[40px] h-[40px] mx-auto'></img>
             </div>
         </div>
-    </Header>
+    </header>
   )
 }
-
-export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      primary: 'Rubik',
-      secondary: 'Rajdhani',
-      tertiary: 'Aldrich',
+      primary: ['Rubik', 'sans-serif'],
     },
     extend: {
       colors: {
