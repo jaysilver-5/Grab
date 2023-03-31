ba44ed8fe080c127bd333b017a4571ed8328cb96
Joshua Udo <73034334+jaysilver-5@users.noreply.github.com>
2023-03-31 17:23:11 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ import Phone from './assets/Phone.png';
 const Header = () => {
   return (
       <div className="container mx-auto w-screen bg-secondary bg-gray-800 h-28">
-        <div className=" mx-auto px-2 w-full py-2 flex justify-between items-center h-full">
+        <div className=" mx-auto px-2 bg-secondary w-full py-2 flex justify-between items-center h-full">
           <div className="flex items-center">
             <img src={Logo} alt="Logo" className="h-[36px] w-30 mr-2" />
           </div>
