9992ea25db38a0e1b955baf6b736267ce70c9091
Joshua Udo <73034334+jaysilver-5@users.noreply.github.com>
2023-03-31 18:02:54 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 function Navbar() {
   return (
     <nav className="container bg-secondary bg-opacity-25 absolute left-0 right-0 text-white z-50 top-28 flex items-center justify-between h-16">
-      <div className="flex items-center w-full">
+      <div className="flex mx-auto items-center w-full">
         <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
           Home
         </a>
