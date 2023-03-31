54860f35eab4bee272f570fe5dd0b33e106b5bcd
Joshua Udo <73034334+jaysilver-5@users.noreply.github.com>
2023-03-31 19:00:20 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,38 +4,40 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <nav className="container bg-secondary bg-opacity-25 absolute left-0 right-0 text-white z-50 top-28 flex items-center justify-between h-16">
-      <div className="flex items-center w-full">
-        <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
-          Home
-        </a>
-        <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
-          About
-        </a>
-        <a href="#" className="px-3.5 text-[14px] flex items-center border-r border-gray-400 hover:text-gray-300">
-          Pages <AiOutlineDown className="" />
-        </a>
-        <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
-          Project
-        </a>
-        <a href="#" className="px-3.5 text-[14px] hover:text-gray-300">
-          Contact
-        </a>
-      </div>
-      <div className="flex items-center">
-        <a href="#">
-          <FaFacebook className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
-        </a>
-        <a href="#">
-          <FaTwitter className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
-        </a>
-        <a href="#">
-          <FaInstagram className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
-        </a>
-        <a href="#">
-          <FaLinkedin className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
-        </a>
-        <div className="ml-5 flex flex-1 items-center text-black w-72 h-16 w-16 bg-white"><h1 className="mx-auto font-secondary font-medium">Request quote</h1></div>
+    <nav className="container bg-secondary bg-opacity-25 absolute left-0 right-0 text-white z-50 top-28 h-16">
+      <div className='flex items-center justify-between'>
+        <div className="flex items-center w-full">
+          <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
+            Home
+          </a>
+          <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
+            About
+          </a>
+          <a href="#" className="px-3.5 text-[14px] flex items-center border-r border-gray-400 hover:text-gray-300">
+            Pages <AiOutlineDown className="" />
+          </a>
+          <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
+            Project
+          </a>
+          <a href="#" className="px-3.5 text-[14px] hover:text-gray-300">
+            Contact
+          </a>
+        </div>
+        <div className="flex items-center">
+          <a href="#">
+            <FaFacebook className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
+          </a>
+          <a href="#">
+            <FaTwitter className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
+          </a>
+          <a href="#">
+            <FaInstagram className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
+          </a>
+          <a href="#">
+            <FaLinkedin className="text-2xl h-[19px] mx-4 hover:text-gray-300" />
+          </a>
+          <div className="ml-5 flex flex-1 items-center text-black w-72 h-16 w-16 bg-white"><h1 className="mx-auto font-secondary font-medium">Request quote</h1></div>
+        </div>
       </div>
     </nav>
   );
