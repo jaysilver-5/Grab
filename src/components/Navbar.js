b6a7abaebb33cc504567a40471a7d61a5eb698f6
Joshua Udo <73034334+jaysilver-5@users.noreply.github.com>
2023-03-31 18:26:03 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,7 @@ import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div className='bg-secondary h-16'>
-    <nav className="container mx-auto absolute w-screen bg-cover bg-opacity-25 text-white z-50 top-28 flex items-center justify-between">
+    <nav className="container mx-auto bg-secondary h-16 absolute w-screen bg-cover bg-opacity-25 text-white z-50 top-28 flex items-center justify-between">
       <div className="flex items-center w-full">
         <a href="#" className="px-3.5 text-[14px] border-r border-gray-400 hover:text-gray-300">
           Home
@@ -43,7 +42,6 @@ function Navbar() {
         <div className="ml-5 flex items-center text-black w-72 h-16 w-16 bg-white"><h1 className="mx-auto font-secondary font-medium">Request quote</h1></div>
       </div>
     </nav>
-    </div>
   );
 }
 
