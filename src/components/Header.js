e825e87a9fbb5870c6188cc2c23672b96f374b1e
Joshua Udo <73034334+jaysilver-5@users.noreply.github.com>
2023-03-31 17:28:06 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,27 +7,28 @@ import Phone from './assets/Phone.png';
 
 const Header = () => {
   return (
-      <div className="container mx-auto w-screen bg-secondary bg-gray-800 h-28">
-        <div className=" mx-auto px-2 bg-secondary w-full py-2 flex justify-between items-center h-full">
-          <div className="flex items-center">
-            <img src={Logo} alt="Logo" className="h-[36px] w-30 mr-2" />
-          </div>
-          <div className="flex items-center">
-            <div className="flex items-center mr-6">
-              <img src={Time} alt='' className="h-[63px] w-[63px]"/>
-              <span className="text-white font-secondary ml-2 text-[12px] font-medium">Mon-Sat. 9.00 - 18.00<br/>Sunday closed Messsage </span>
-            </div>
-            <div className="flex items-center mr-6">
-              <img src={Email} alt='' className="h-[63px] w-[63px]"/>
-              <span className="text-white font-secondary ml-2 text-[12px] font-medium">Email<br/>contact@logistics.com</span>
+       <div className='bg-secondary'>
+        <div className="container mx-auto w-screen bg-secondary bg-gray-800 h-28">
+          <div className=" mx-auto px-2 bg-secondary w-full py-2 flex justify-between items-center h-full">
+            <div className="flex items-center">
+              <img src={Logo} alt="Logo" className="h-[36px] w-30 mr-2" />
             </div>
             <div className="flex items-center">
-              <img src={Phone} alt='' className="h-[63px] w-[63px]"/>
-              <span className="text-white font-secondary ml-2 text-[12px] font-medium">Call us<br/>(00) 112 365 489</span>
+              <div className="flex items-center mr-6">
+                <img src={Time} alt='' className="h-[63px] w-[63px]"/>
+                <span className="text-white font-secondary ml-2 text-[12px] font-medium">Mon-Sat. 9.00 - 18.00<br/>Sunday closed Messsage </span>
+              </div>
+              <div className="flex items-center mr-6">
+                <img src={Email} alt='' className="h-[63px] w-[63px]"/>
+                <span className="text-white font-secondary ml-2 text-[12px] font-medium">Email<br/>contact@logistics.com</span>
+              </div>
+              <div className="flex items-center">
+                <img src={Phone} alt='' className="h-[63px] w-[63px]"/>
+                <span className="text-white font-secondary ml-2 text-[12px] font-medium">Call us<br/>(00) 112 365 489</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
   );
 };
 
